9cfddf699a3f008e3c916ce14f67b19804aa5d9c
Cheik H Bagagnan <bchamidou@gmail.com>
2023-07-10 16:30:12 -0400

    update Module05

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,13 @@
 // Set the date at the top of the page 
 
-var d=dayjs().format('MMM D, YYYY')
+var d=dayjs().format('MMM D, YYYY');
 
 $("#currentDay").text(dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'));
 
 $(document).ready(function () {
 
   timeStatus(); 
+  renderText ();
 
 });
   
@@ -15,11 +16,14 @@ $(document).ready(function () {
 
     // Save the data to localStorage on click
   $(".saveBtn").on("click", function() {
-    timeBlock = $(this).parent().attr("id");
+    timeBlock = $(this).siblings(".hour").text();
     console.log(timeBlock);
     description = $(this).siblings(".description").val();
     console.log(description);
     localStorage.setItem(timeBlock, JSON.stringify(description));
+    
+    timeStatus(); 
+    renderText ();
     
   });
 
@@ -29,8 +33,9 @@ $(document).ready(function () {
     description = $(this).siblings(".description").val();
     timeBlock = $(this).siblings(".hour").text();
     localStorage.setItem(timeBlock, JSON.stringify(description));
-
+     
     timeStatus(); 
+    renderText ();
 
   });
   
@@ -40,38 +45,58 @@ $(document).ready(function () {
     console.log("Current Time" +  timeNow);
   
     // loop over time blocks to know if they are past, present or future
+    $(".description").each(function () {
+      var timeBlock = parseInt($(this).attr("id"));
+      console.log(timeBlock);
 
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
-        console.log(blockTime);
-
-        if (blockTime < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
+      if (timeBlock < timeNow) {
+          $(this).addClass("past");
+        } else if (timeBlock === timeNow) {
+          $(this).addClass("present");
+        } else {
+          $(this).addClass("future");
         }
-        else if (blockTime === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-  
-        }
-    })
+    });
   }  
+  
+//  // Load any saved data from localStorage
 
- //
- // Load any saved data from localStorage
-
-$(".description").each(function() {
-  var timeBlock = $(this).parent().attr("id");
-  var savedData = localStorage.getItem(timeBlock); 
-  if (savedData) {
-    $(this).val(savedData);
-  }
-});
+    function renderText () {   
+      var saveEventText9 = JSON.parse(localStorage.getItem("9AM"));
+    $("#9").val("");
+    $("#9").val(saveEventText9);
+  
+    var saveEventText10 = JSON.parse(localStorage.getItem("10AM"));
+    $("#10").val("");
+    $("#10").val(saveEventText10);
+    
+    var saveEventText11 = JSON.parse(localStorage.getItem("11AM"));
+    $("#11").val("");
+    $("#11").val(saveEventText11);
+    
+    var saveEventText12 = JSON.parse(localStorage.getItem("12AM"));
+    $("#12").val("");
+    $("#12").val(saveEventText12);
+    
+    var saveEventText1 = JSON.parse(localStorage.getItem("1PM"));
+    $("#13").val("");
+    $("#13").val(saveEventText1);
+  
+    var saveEventText2 = JSON.parse(localStorage.getItem("2PM"));
+    $("#14").val("");
+    $("#14").val(saveEventText2);
+  
+    var saveEventText3 = JSON.parse(localStorage.getItem("3PM"));
+    $("#15").val("");
+    $("#15").val(saveEventText3);
+  
+    var saveEventText4 = JSON.parse(localStorage.getItem("4PM"));
+    $("#16").val("");
+    $("#16").val(saveEventText4);
+  
+    var saveEventText5 = JSON.parse(localStorage.getItem("5PM"));
+    $("#17").val("");
+    $("#17").val(saveEventText5);
+  
+  };
 
--- README.md
+++ README.md
@@ -2,28 +2,32 @@
 
 This repository create a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-Deployed program: https://bchamidou.github.io/Work_Day_Scheduler/
+## Description
+
+Create a simple calendar application that allows the user to save events for each hour of the day.  
+ The calendar show the hours from 9am to 5pm, user click in the box and add your text, the save button will save the text, even upon refresh or closing the browser and coming back. The delete button will delete the item from the calendar completely. The blocks of time will recognize if they are in the past, present or future and are color coded thusly.
 
 Technologies Used: HTML, Bootstrap, CSS, JavaScript, moment.js, jQuery
 
- The calendar was supposed to show the hours from 9am to 5pm. I extended this from 8am - 8pm for further flexibility. The calendar is intuitive, click in the box and add your text, the save button will save the text, even upon refresh or closing the browser and coming back. The delete button will delete the item from the calendar completely. The blocks of time will recognize if they are in the past, present or future and are color coded thusly.
+Day.js (https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
 
-I utilized several coding websites for help, including StackOverflow and w3schools.com I also watched YouTube tutorials and got assistance from fellow student Aurora Brune.
+## Usage
 
-image
+Deployed program: https://bchamidou.github.io/Work_Day_Scheduler/
 
-## Your Task
+The following give instructions and examples for use. Include screenshots as needed. 
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+![Alt text](Assets/images%2012.17.38/Day_scheduler.png)
 
-You'll need to use the Moment.js library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+I utilized several coding websites for help, including StackOverflow and w3schools.com .... I also watched YouTube tutorials and the course resources.
 
 ## User Story
 
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
-Acceptance Criteria
+
+## Acceptance Criteria
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
@@ -37,15 +41,11 @@ WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
-The following animation demonstrates the application functionality:
 
 ## Mock-Up
 
-![Alt text](Assets/05-third-party-apis-homework-demo.gif)
-
-Review
-You are required to submit the following for review:
-
-The URL of the deployed application.
+![The following animation demonstrates the application functionality:](Assets/images%2012.17.38/05-third-party-apis-homework-demo.gif)
 
 The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+ 
